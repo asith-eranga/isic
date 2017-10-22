661879bbf5b67cdd2e503da440d8b6311e131b64
asitheranga <asith2u@yahoo.com>
2017-10-22 14:47:31 +0530

    move menu to new file

---
+++ partials/main-menu.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * User: Asith
+ * Date: 22-Oct-17
+ * Time: 2:37 PM
+ */
+
+?>
+
+<div id="menu-main" class="menu main-menu-wrapper show-search-item show-off-canvas menu-actions-btn-width-2" role="navigation">
+    <div class="main-menu-inner">
+        <div class="content-wrap">
+            <div class="">
+                <nav class="main-menu-container">
+                    <ul id="main-navigation" class="main-menu menu bsm-pure clearfix">
+                        <li class="menu-item menu-item-object-page better-anim-fade "><a href="#">Home</a></li>
+                        <li class="menu-item menu-item-object-page "><a href="#">About</a></li>
+                        <li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children ">
+                            <a href="#">Cards</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item menu-item-object-page better-anim-fade "><a href="#">Students</a></li>
+                                <li class="menu-item better-anim-fade "><a href="#">Youth</a></li>
+                                <li class="menu-item  better-anim-fade "><a href="#">Teachers</a></li>
+                            </ul>
+                        </li>
+                        <li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children current-menu-item current_page_item ">
+                            <a href="#">Discounts</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item menu-item-object-page current-menu-item current_page_item better-anim-fade "><a href="#">Flights</a></li>
+                                <li class="menu-item better-anim-fade "><a href="#">Hotels</a></li>
+                                <li class="menu-item  better-anim-fade "><a href="#">Visas</a></li>
+                                <li class="menu-item  better-anim-fade "><a href="#">Rail</a></li>
+                                <li class="menu-item  better-anim-fade "><a href="#">Transfers</a></li>
+                                <li class="menu-item  better-anim-fade "><a href="#">Insurance</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item menu-item-type-taxonomy menu-item-object-category  better-anim-fade "><a href="#"> Partner with isic </a></li>
+                        <li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children ">
+                            <a href="#">Travel With us</a>
+                            <ul class="sub-menu">
+                                <li class="menu-item menu-item-object-page better-anim-fade "><a href="#">Flights</a></li>
+                                <li class="menu-item better-anim-fade "><a href="#">Hotels</a></li>
+                                <li class="menu-item  better-anim-fade "><a href="#">Visas</a></li>
+                                <li class="menu-item  better-anim-fade "><a href="#">Rail</a></li>
+                                <li class="menu-item  better-anim-fade "><a href="#">Transfers</a></li>
+                                <li class="menu-item  better-anim-fade "><a href="#">Insurance</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item menu-item-type-taxonomy menu-item-object-category  better-anim-fade "><a href="#">Take a va-cay</a></li>
+                        <li class="menu-item menu-item-type-taxonomy menu-item-object-category  better-anim-fade "><a href="#"> contact </a></li>
+                    </ul>
+                    <div class="menu-action-buttons bg-green col-md-3">
+                        <div class="off-canvas-menu-icon-container off-icon-right">
+                            <div class="off-canvas-menu-icon">
+                                <h5 class="txt-yellow">GET YOUR CARD</h5>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</div>
