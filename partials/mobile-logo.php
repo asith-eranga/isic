b23b32c73c3668b16f86a2f20680c45609688a4f
asitheranga <asith2u@yahoo.com>
2017-10-22 18:02:47 +0530

    create mobile logo common file

---
+++ partials/mobile-logo.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: Asith
+ * Date: 22-Oct-17
+ * Time: 5:59 PM
+ */
+?>
+
+<div class="rh-header clearfix light deferred-block-exclude">
+    <div class="rh-container clearfix">
+        <div class="menu-container close">
+            <span class="menu-handler"><span class="lines"></span></span>
+        </div>
+        <div class="logo-container">
+            <a href="#" itemprop="url" rel="home">
+                <img src="<?php echo HTTP_PATH; ?>images/isic2_logo.png" alt="" />
+            </a>
+        </div>
+    </div>
+</div>
