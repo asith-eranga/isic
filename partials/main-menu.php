714e16ad09a4cdcf2a6661ceea726addad88190c
asitheranga <asith2u@yahoo.com>
2017-10-24 00:12:29 +0530

    set current menu

--- partials/main-menu.php
+++ partials/main-menu.php
@@ -4,6 +4,11 @@
  * Date: 22-Oct-17
  * Time: 2:37 PM
  */
+
+$current_path = $_SERVER['REQUEST_URI'];
+$current_path = rtrim($current_path, '/');
+$current_path = end(explode('/',$current_path));
+$current_page_class = 'current-menu-item current_page_item';
 ?>
 
 <div id="menu-main" class="menu main-menu-wrapper show-search-item show-off-canvas menu-actions-btn-width-2" role="navigation">
@@ -12,8 +17,8 @@
             <div class="">
                 <nav class="main-menu-container">
                     <ul id="main-navigation" class="main-menu menu bsm-pure clearfix">
-                        <li class="menu-item menu-item-object-page better-anim-fade current-menu-item current_page_item "><a href="<?php  echo HTTP_PATH; ?>">Home</a></li>
-                        <li class="menu-item menu-item-object-page "><a href="<?php echo HTTP_PATH; ?>about">About</a></li>
+                        <li class="menu-item menu-item-object-page better-anim-fade <?php if ($current_path == 'isic') { echo $current_page_class; } ?>"><a href="<?php  echo HTTP_PATH; ?>">Home</a></li>
+                        <li class="menu-item menu-item-object-page <?php if ($current_path == 'about') { echo $current_page_class; } ?>"><a href="<?php echo HTTP_PATH; ?>about">About</a></li>
                         <li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children ">
                             <a href="<?php echo HTTP_PATH; ?>cards">Cards</a>
                             <ul class="sub-menu">
@@ -22,7 +27,7 @@
                                 <li class="menu-item  better-anim-fade "><a href="#">Teachers</a></li>
                             </ul>
                         </li>
-                        <li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children">
+                        <li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children <?php if ($current_path == 'discount') { echo $current_page_class; } ?>">
                             <a href="<?php echo HTTP_PATH; ?>discount">Discounts</a>
                             <ul class="sub-menu">
                                 <li class="menu-item menu-item-object-page current-menu-item current_page_item better-anim-fade "><a href="#">Flights</a></li>
@@ -34,7 +39,7 @@
                             </ul>
                         </li>
                         <li class="menu-item menu-item-type-taxonomy menu-item-object-category  better-anim-fade "><a href="#"> Partner with isic </a></li>
-                        <li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children ">
+                        <li  class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children <?php if ($current_path == 'travel-with-us') { echo $current_page_class; } ?>">
                             <a href="<?php echo HTTP_PATH; ?>travel-with-us">Travel With us</a>
                             <ul class="sub-menu">
                                 <li class="menu-item menu-item-object-page better-anim-fade "><a href="#">Flights</a></li>
@@ -46,7 +51,7 @@
                             </ul>
                         </li>
                         <li class="menu-item menu-item-type-taxonomy menu-item-object-category  better-anim-fade "><a href="#">Take a va-cay</a></li>
-                        <li class="menu-item menu-item-type-taxonomy menu-item-object-category  better-anim-fade "><a href="<?php echo HTTP_PATH; ?>contact"> contact </a></li>
+                        <li class="menu-item menu-item-type-taxonomy menu-item-object-category  better-anim-fade <?php if ($current_path == 'contact') { echo $current_page_class; } ?>"><a href="<?php echo HTTP_PATH; ?>contact"> contact </a></li>
                     </ul>
                     <div class="menu-action-buttons bg-green col-md-3">
                         <div class="off-canvas-menu-icon-container off-icon-right">
