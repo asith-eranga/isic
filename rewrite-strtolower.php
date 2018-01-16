9e01051d667abda4f13eae3ef57c0c397f47433e
asitheranga <asith2u@yahoo.com>
2018-01-16 21:50:16 +0530

    redirect all urls to lowercase

---
+++ rewrite-strtolower.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Asith
+ * Date: 16-Jan-18
+ * Time: 9:44 PM
+ */
+
+if(isset($_GET['rewrite-strtolower-url'])) {
+    $url = $_GET['rewrite-strtolower-url'];
+    unset($_GET['rewrite-strtolower-url']);
+    $params = strtolower(http_build_query($_GET));
+    if(strlen($params)) {
+        $params = '?' . $params;
+    }
+    header('Location: http://' . $_SERVER['HTTP_HOST'] . '/isic/' . strtolower($url) . $params, true, 301);
+    exit;
+}
+header("HTTP/1.0 404 Not Found");
+die('Unable to convert the URL to lowercase. You must supply a URL to work upon.');
